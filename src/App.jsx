30902d4967990d43167372a76d06f18ff3a48aa7
Kopikanth <118206393+Kopikanth@users.noreply.github.com>
2025-09-17 13:21:52 +0530

    finished footer and created a custom animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
@@ -6,20 +6,94 @@ import Services from "./components/Services";
 import OurWork from "./components/OurWork";
 import Team from "./components/Team";
 import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
 
   const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
 
+  const ringRef = useRef(null);
+  const dotRef = useRef(null);
+  const mouse = useRef({ x: 0, y: 0 });
+  const position = useRef({ x: 0, y: 0 });
+
+  // const moveCursor = (event) => {
+  //   // console.log(event.clientX);
+  //   ringRef.current.style.top = event.clientY + 'px';
+  //   ringRef.current.style.left = event.clientX + 'px';
+  //   dotRef.current.style.top = event.clientY + 'px';
+  //   dotRef.current.style.left = event.clientX + 'px';
+  // }
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      mouse.current.x = e.clientX;
+      mouse.current.y = e.clientY;
+    }
+
+    const handleMouseEnter = (e) => {
+      if (dotRef.current && ringRef.current) {
+        dotRef.current.style.display = 'block';
+        ringRef.current.style.display = 'block';
+        // dotRef.current.style.top = `${e.clientY}px`;
+        // dotRef.current.style.left = `${e.clientX}px`;
+        // ringRef.current.style.top = `${e.clientY}px`;
+        // ringRef.current.style.left = `${e.clientX}px`;
+        mouse.current.x = e.clientX;
+        mouse.current.y = e.clientY;
+        position.current.x = e.clientX;
+        position.current.y = e.clientY;
+      }
+    }
+
+    const handleMouseOut = () => {
+      dotRef.current.style.display = 'none';
+      ringRef.current.style.display = 'none';
+    }
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseenter', handleMouseEnter);
+    document.addEventListener('mouseleave', handleMouseOut);
+
+    const animate = () => {
+      position.current.x += (mouse.current.x - position.current.x) * 0.1;
+      position.current.y += (mouse.current.y - position.current.y) * 0.1;
+
+      if (dotRef.current && ringRef.current) {
+        dotRef.current.style.transform = `translate3d(${mouse.current.x - 6}px, ${mouse.current.y - 6}px, 0)`;
+        ringRef.current.style.transform = `translate3d(${position.current.x - 20}px, ${position.current.y - 20}px, 0)`;
+      }
+
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseenter', handleMouseEnter);
+      document.removeEventListener('mouseleave', handleMouseOut);
+    }
+  }, [])
+
+
   return (
     <div className="dark:bg-black relative">
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
-      <Hero/>
-      <TrustedBy/>
-      <Services/>
-      <OurWork/>
-      <Team/>
-      <ContactUs/>
+      <Hero />
+      <TrustedBy />
+      <Services />
+      <OurWork />
+      <Team />
+      <ContactUs />
+      <Footer theme={theme} />
+
+      {/* custom cursor ring */}
+      <div ref={ringRef} className={"fixed hidden top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"} />
+
+      {/* custom cursor dot */}
+      <div ref={dotRef} className="fixed hidden top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]" />
     </div>
   );
 };
--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,32 +1,60 @@
+import toast from "react-hot-toast";
 import assets from "../assets/assets";
 import Title from "./Title";
 
 const ContactUs = () => {
 
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append('access_key', '07d01a0c-d719-4527-88d6-759d26212c3d');
+
+        try {
+            const response = await fetch('https://api.web3forms.com/submit', {
+                method: "POST",
+                body: formData
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                toast.success(data.message);
+                event.target.reset();
+            }
+            else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+
+    }
+
     return (
         <div id="contact-us" className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
             <Title title='Reach out to us' desc="Ready to grow your brand? Let's connect and build something exceptional together." />
-            <form className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+            <form onSubmit={onSubmit} className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
                 <div>
                     <p className="mb-2 text-sm font-medium">Your name</p>
                     <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
                         <img src={assets.person_icon} alt="person icon" />
-                        <input type="text" className="w-full p-3 text-sm outline-none" placeholder="Enter your name" />
+                        <input name="name" required type="text" className="w-full p-3 text-sm outline-none" placeholder="Enter your name" />
                     </div>
                 </div>
                 <div>
                     <p className="mb-2 text-sm font-medium">Email id</p>
                     <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
                         <img src={assets.email_icon} alt="email icon" />
-                        <input type="email" className="w-full p-3 text-sm outline-none" placeholder="Enter your email" />
+                        <input name="email" required type="email" className="w-full p-3 text-sm outline-none" placeholder="Enter your email" />
                     </div>
                 </div>
                 <div className="sm:col-span-2">
                     <p className="mb-2 text-sm font-medium">Message</p>
-                    <textarea placeholder="Enter your message" rows={8} className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"/>
+                    <textarea name="message" required placeholder="Enter your message" rows={8} className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600" />
                 </div>
                 <button type="submit" className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-transform">
-                    Submit <img src={assets.arrow_icon} alt="arrow icon" className="w-4"/>
+                    Submit <img src={assets.arrow_icon} alt="arrow icon" className="w-4" />
                 </button>
             </form>
         </div>
